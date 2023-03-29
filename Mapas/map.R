5e0ae1f49b135faaa12294b2366a59a79442d86e
gusgarciacruz <53344592+gusgarciacruz@users.noreply.github.com>
2023-03-29 08:14:45 -0500

    Add files via upload

--- Mapas/map.R
+++ Mapas/map.R
@@ -1,14 +1,14 @@
 library(sf); library(tmap); library(tidyverse); library(readxl); library(tmaptools)
-library(viridis)
+library(viridis); library(summarytools); library(terra)
 
-setwd("C:/Users/ggarci24/OneDrive - Universidad EAFIT/EAFIT/Cursos EAFIT/Econometría avanzada pregrado/Mapas R")
+setwd("C:/Users/ggarci24/OneDrive - Universidad EAFIT/EAFIT/Cursos EAFIT/EconometrÃ­a avanzada pregrado/Mapas R")
 
 map_comunas_corr <- st_read("LimiteComunaCorregimiento_2014.shp") %>%
   rename(idcomuna=CODIGO) %>%
   filter(NOMBRE!='NA')
 
-tm_shape(map_comunas_corr)+
-  tm_borders(col='blue') 
+ggplot(map_comunas_corr) +
+  geom_sf(color = "red", size = 0.05)
 
 map_comunas <- st_read("LimiteComunaCorregimiento_2014.shp") %>%
   rename(idcomuna=CODIGO) %>%
@@ -24,24 +24,27 @@ map_comunas <- st_read("LimiteComunaCorregimiento_2014.shp") %>%
                              NOMBRE=='Buenos Aires'~5.9963,
                              NOMBRE=='La Candelaria'~7.3563,
                              NOMBRE=='Laureles Estadio'~7.42,
-                             NOMBRE=='La América'~3.89,
+                             NOMBRE=='La AmÃ©rica'~3.89,
                              NOMBRE=='San Javier'~7,
                              NOMBRE=='El Poblado'~23,
                              NOMBRE=='Guayabal'~7.64,
-                             NOMBRE=='Belén'~8.312)) 
+                             NOMBRE=='BelÃ©n'~8.312)) 
 
 names(map_comunas)
 
-cbind(table(map_comunas$idcomuna))
+summarytools::freq(map_comunas$idcomuna)
 
-tm_shape(map_comunas)+
-  tm_borders(col='red') 
+ggplot(map_comunas) +
+  geom_sf(color = "red", size = 0.05)
 
 map_metro <- st_read("LineasSistema_07-20.shp") %>%
             filter(LINEA != "La Aldea")  
 
+ggplot(map_metro) +
+  geom_sf(color = "blue", size = 0.05)
+
 data <- read_excel("ProyeccionComuna.xlsx") %>%
-  rename(comuna=Comuna, totalpob="Total Población",
+  rename(comuna=Comuna, totalpob="Total PoblaciÃ³n",
          percent="% de total SUM([hombres]) + SUM([mujeres]) junto con Comuna...4") %>%
   select(comuna, totalpob, percent) %>%
   filter(comuna!="Total") %>%
@@ -56,20 +59,19 @@ data <- read_excel("ProyeccionComuna.xlsx") %>%
                               comuna=='Buenos Aires'~'09',
                               comuna=='La Candelaria'~'10',
                               comuna=='Laureles - Estadio'~'11',
-                              comuna=='La América'~'12',
+                              comuna=='La AmÃ©rica'~'12',
                               comuna=='San Javier'~'13',
                               comuna=='El Poblado'~'14',
                               comuna=='Guayabal'~'15',
-                              comuna=='Belén'~'16',
+                              comuna=='BelÃ©n'~'16',
                               comuna=='Palmitas'~'50',
-                              comuna=='San Cristóbal'~'60',
+                              comuna=='San CristÃ³bal'~'60',
                               comuna=='Altavista'~'70',
                               comuna=='San Antonio'~'80',
                               comuna=='Santa Elena'~'90'))
                               
 names(data)
-head(map[,c("NOMBRE","CODIGO")])
-tail(map[,c("NOMBRE","CODIGO")], 10)
+head(map_comunas[,c("NOMBRE","idcomuna")])
 
 map_comunas_merge <- left_join(map_comunas,data) %>%
                      mutate(densidad = as.integer(totalpob/areakm2)) 
@@ -87,7 +89,7 @@ m1 <-  tm_shape(map_comunas_merge) +
     tm_text("comuna", col="gray30", scale=.7) +
     tm_compass(type = "8star", position = c("right", "bottom")) +
     tm_shape(map_metro)+tm_lines(col = 'red', lwd = 1.5, legend.show = TRUE) +
-    tm_add_legend("line", col='red', labels=c("Línea de Metro")) +
+    tm_add_legend("line", col='red', labels=c("LÃ­nea de Metro")) +
     tm_layout(legend.format = list(text.separator = "-"), 
             legend.position = c("left","bottom"),legend.text.size = .8)
 m1
@@ -118,13 +120,13 @@ theme_set(theme_void())
 
 ggplot(map_comunas_merge) +
   geom_sf(aes(fill = densidad), color = "#ffffff", size = 0.05)  +
-  geom_sf_text(aes(label = comuna), size=1.2) +
-  geom_sf(data = map_metro, fill = "grey40", aes(color = "Líneal de metro"), size=.4) +
+  geom_sf_text(aes(label = comuna), size=0.8) +
+  geom_sf(data = map_metro, fill = "grey40", aes(color = "LÃ­neal de metro"), size=.2) +
   scale_fill_viridis_c(direction = -1) + labs(fill=NULL) +
-  scale_color_manual(values = c("Líneal de metro" = "grey40"), name=NULL) +
+  scale_color_manual(values = c("LÃ­neal de metro" = "grey40"), name=NULL) +
   theme(legend.position = c(.8,.25),
         legend.key.size = unit(0.15,"cm"),
-        legend.text=element_text(size=3),
+        legend.text=element_text(size=2),
         legend.spacing = unit(-.1,"cm"),
         legend.spacing.x = unit(.04, 'cm'))
 
